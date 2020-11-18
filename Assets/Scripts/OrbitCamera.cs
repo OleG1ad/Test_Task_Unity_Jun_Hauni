94fe80a0601e9c110c2b7b04f492610d232b25ca
OleG1ad <gladenkov.gov@gmail.com>
2020-11-19 00:51:40 +0200

    OrbitCamera

---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField]
+    Transform focus = default;
+
+    [SerializeField, Range(1f, 20f)]
+    float distance = 5f;
+
+    [SerializeField, Min(0f)]
+    float focusRadius = 1f;
+
+    Vector3 focusPoint;
+
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.5f;
+
+    Vector2 orbitAngles = new Vector2(45f, 0f);
+
+    // вручную управлять орбитой
+    [SerializeField, Range(1f, 360f)]
+    float rotationSpeed = 90f;
+
+    // ограничить минимальный и максимальный вертикальный угол, 
+    //с крайними значениями, ограниченными максимум 89 ° в любом направлении
+    [SerializeField, Range(-89f, 89f)]
+    float minVerticalAngle = -30f, maxVerticalAngle = 60f;
+
+    //настраиваемая задержка выравнивания камеры
+    [SerializeField, Min(0f)]
+    float alignDelay = 5f;
+
+    float lastManualRotationTime;
+
+    void Awake()
+    {
+        focusPoint = focus.position;
+        // начальное вращение соответствует углам орбиты
+        transform.localRotation = Quaternion.Euler(orbitAngles);
+    }
+
+    void LateUpdate()
+    {
+        UpdateFocusPoint();
+        // нужно пересчитать поворот только в том случае, 
+        //если было изменение, иначе мы можем получить существующее
+        Quaternion lookRotation;
+        if (ManualRotation() || AutomaticRotation())
+        {
+            ConstrainAngles();
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }
+        else
+        {
+            lookRotation = transform.localRotation;
+        }
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+
+    void UpdateFocusPoint()
+    {
+        Vector3 targetPoint = focus.position;
+        if (focusRadius > 0f)
+        {
+            float distance = Vector3.Distance(targetPoint, focusPoint);
+            float t = 1f;
+            if (distance > 0.01f && focusCentering > 0f)
+            {
+                t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
+            }
+            if (distance > focusRadius)
+            {
+                t = Mathf.Min(t, focusRadius / distance);
+            }
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
+        }
+        else
+        {
+            focusPoint = targetPoint;
+        }
+    }
+
+    bool ManualRotation()
+    {
+        Vector2 input = new Vector2(
+            Input.GetAxis("Vertical Camera"),
+            Input.GetAxis("Horizontal Camera")
+        );
+        const float e = 0.001f;
+        if (input.x < e || input.x > e || input.y < e || input.y > e)
+        {
+            orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+            lastManualRotationTime = Time.unscaledTime;
+            return true;
+        }
+        return false;
+    }
+
+
+    //Максимальное значение никогда не должно опускаться ниже минимального
+    void OnValidate()
+    {
+        if (maxVerticalAngle < minVerticalAngle)
+        {
+            maxVerticalAngle = minVerticalAngle;
+        }
+    }
+
+    // угол остается в диапазоне 0–360
+    void ConstrainAngles()
+    {
+        orbitAngles.x =
+            Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+
+        if (orbitAngles.y < 0f)
+        {
+            orbitAngles.y += 360f;
+        }
+        else if (orbitAngles.y >= 360f)
+        {
+            orbitAngles.y -= 360f;
+        }
+    }
+
+    // прерывается, если текущее время минус время последнего ручного вращения меньше, 
+    //чем задержка выравнивания
+    bool AutomaticRotation()
+    {
+        if (Time.unscaledTime - lastManualRotationTime < alignDelay)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
